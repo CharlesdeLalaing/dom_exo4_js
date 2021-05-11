919a600bc3578d8ebbf9efa0a0e45456e380000d
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2021-05-11 10:42:30 +0200

    push correction exo4

--- main.js
+++ main.js
@@ -27,3 +27,30 @@ Array.prototype.forEach.call(toutLi, function(el) {
     // Do stuff here
     console.log(el.innerText.toUpperCase());
 });
+
+//correction 
+
+//exo1
+let exo1 = document.getElementsByTagName('h1')[1].innerText;
+console.log(exo1);
+
+//exo2
+let exo2 = document.getElementsByTagName('li')[3].innerText;
+console.log(exo2);
+
+//exo3
+let exo3 = document.getElementsByTagName('p')[0].innerText.toUpperCase();
+console.log(exo3);
+
+//exo4 - loic - BONNE METHODE !
+let exo4 = document.getElementsByTagName('ul')[0];
+let exo4Bis = Array.from(exo4.getElementsByTagName('li'));
+exo4Bis.forEach(li => {
+    console.log(li.innerText.toUpperCase())
+})
+
+//exo4 - farah
+Object.entries(toutLi).forEach(e => {
+    console.log(e[1].innerText.toUpperCase())
+})
+//le 1 prend juste le mot, dans un li ya une point avant le mot, du coup le 1 est pour le mot.
